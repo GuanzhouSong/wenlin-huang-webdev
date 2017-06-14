1f6726ebd787c410422dfc9eaa1b346e0bbfb257
Wenlin Huang <jwong8593@gmail.com>
2017-06-14 08:42:47 -0700

    Moved routing to a separate config.js file from app.js

--- public/assignment/app.js
+++ public/assignment/app.js
@@ -3,28 +3,5 @@
  */
 (function () {
     angular
-        .module("webAppMaker", ["ngRoute"])  // only declare the module in app.js
-        .config(configuration);
-
-    function configuration($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'home.html'
-            })
-            .when('/login', {
-                templateUrl: 'views/user/templates/login.view.client.html',
-                controller: 'loginController',
-                controllerAs: 'model'
-            })
-            .when('/register', {
-                templateUrl: 'views/user/templates/register.view.client.html',
-                controller: 'loginController',
-                controllerAs: 'model'
-            })
-            .when('/profile/:userId', {
-                templateUrl: 'views/user/templates/profile.view.client.html',
-                controller: 'profileController',
-                controllerAs: 'model'
-            })
-    }
+        .module("webAppMaker", ["ngRoute"]);  // only declare the module in app.js
 })();
---
+++ public/assignment/config.js
@@ -0,0 +1,30 @@
+/**
+ * Created by Jeremy on 6/14/17.
+ */
+(function () {
+    angular
+        .module("webAppMaker")
+        .config(configuration);
+
+    function configuration($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'home.html'
+            })
+            .when('/login', {
+                templateUrl: 'views/user/templates/login.view.client.html',
+                controller: 'loginController',
+                controllerAs: 'model'
+            })
+            .when('/register', {
+                templateUrl: 'views/user/templates/register.view.client.html',
+                controller: 'loginController',
+                controllerAs: 'model'
+            })
+            .when('/profile/:userId', {
+                templateUrl: 'views/user/templates/profile.view.client.html',
+                controller: 'profileController',
+                controllerAs: 'model'
+            })
+    }
+})();

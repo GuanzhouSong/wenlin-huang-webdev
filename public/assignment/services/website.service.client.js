645dfbfb08d65d27c84b55324d133a745ab09068
Wenlin Huang <jwong8593@gmail.com>
2017-06-15 10:43:04 -0700

    Encapsulate website data and behaviors in website service

---
+++ public/assignment/services/website.service.client.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Jeremy on 6/15/17.
+ */
+
+(function () {
+    angular
+        .module("webAppMaker")
+        .service("websiteService", websiteService);
+
+    function websiteService() {
+        this.findAllWebsitesForUser = findAllWebsitesForUser;
+
+        var websites = [  // private variable inside the service
+            { "_id": "123", "name": "Facebook", "developerId": "456", "description": "Lorem" },
+            { "_id": "234", "name": "Tweeter", "developerId": "456", "description": "Lorem" },
+            { "_id": "456", "name": "Gizmodo", "developerId": "456", "description": "Lorem" },
+            { "_id": "890", "name": "Go", "developerId": "123", "description": "Lorem" },
+            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
+            { "_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem" },
+            { "_id": "789", "name": "Chess", "developerId": "234", "description": "Lorem" }
+        ];
+
+        function findAllWebsitesForUser(userId) {
+            var res = [];
+            for (var w in websites)
+                if (websites[w].developerId === userId)
+                    res.push(websites[w]);
+            return res;
+        }
+    }
+})();
--- public/assignment/views/website/controllers/website-list.controller.client.js
+++ public/assignment/views/website/controllers/website-list.controller.client.js
@@ -7,27 +7,11 @@
         .module('webAppMaker')
         .controller('websiteListController', websiteListController);
 
-    function websiteListController($routeParams) {
+    function websiteListController($routeParams, websiteService) {
         var model = this;
-        var websites = [
-            { "_id": "123", "name": "Facebook", "developerId": "456", "description": "Lorem" },
-            { "_id": "234", "name": "Tweeter", "developerId": "456", "description": "Lorem" },
-            { "_id": "456", "name": "Gizmodo", "developerId": "456", "description": "Lorem" },
-            { "_id": "890", "name": "Go", "developerId": "123", "description": "Lorem" },
-            { "_id": "567", "name": "Tic Tac Toe", "developerId": "123", "description": "Lorem" },
-            { "_id": "678", "name": "Checkers", "developerId": "123", "description": "Lorem" },
-            { "_id": "789", "name": "Chess", "developerId": "234", "description": "Lorem" }
-        ];
 
         var userId = $routeParams["userId"];
-        model.websites = findAllWebsitesForUser(userId);
+        model.websites = websiteService.findAllWebsitesForUser(userId);
 
-        function findAllWebsitesForUser(userId) {
-            var res = [];
-            for (var w in websites)
-                if (websites[w].developerId === userId)
-                    res.push(websites[w]);
-            return res;
-        }
     }
 })();
